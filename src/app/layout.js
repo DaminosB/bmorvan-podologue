2a1e51bce00e967df1f9058b69cee0f2ebe7e65d
Damien Bourcheix <damien@mbp-de-damien.home>
2024-01-04 11:09:24 +0100

    Analytics package added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import { Analytics } from "@vercel/analytics/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
